d118cb7f0513591d1ba4b33aa63981ad91871111
emmanuelagyapong <emmanuelagyapong070@gmail.com>
2020-05-12 18:15:52 +0000

    12/05/2020

--- application/__init__.py
+++ application/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 import os
 from flask_sqlalchemy import SQLAlchemy
-
+from os import getenv
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 db=SQLAlchemy(app)
 
--- application/routes.py
+++ application/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
-from application import app
+from flask import render_template, redirect, url_for
+from application import app,db
 from application.models import Posts
+from application.forms import PostForm
 
 blogData = [
     {  
@@ -18,8 +19,8 @@ blogData = [
 @app.route('/')
 @app.route('/home')
 def home():
-    postData = Posts.query.first()
-    return render_template('home.html', title='Home', post=postData)
+    postData = Posts.query.all()
+    return render_template('home.html', title='Home', posts=postData)
 
 @app.route('/about')
 def about():
@@ -33,6 +34,39 @@ def login():
 @app.route('/register')
 def register():
     return render_template('register.html', title="Register page")
+
+@app.route('/post', methods=['GET', 'POST'])
+def post():
+    form = PostForm()
+    if form.validate_on_submit():
+        postData = Posts(
+            first_name = form.first_name.data,
+            last_name = form.last_name.data,
+            title = form.title.data,
+            content = form.content.data
+        )
+
+        db.session.add(postData)
+        db.session.commit()
+
+        return redirect(url_for('home'))
+
+    else:
+        print(form.errors)
+
+    return render_template('post.html', title='Post', form=form)
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
